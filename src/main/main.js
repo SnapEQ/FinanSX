effb1574632227595c780e0ee28a68813410bfb5
SnapEQ <woo.biink@gmail.com>
2024-12-23 13:08:14 +0100

    added few funcionality blueprints

--- src/main/main.js
+++ src/main/main.js
@@ -1,3 +1,31 @@
-const {app, BrowserWindows} = require('electron');
-const path = require('paht');
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
+
+
+const createWindow = () =>{
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences:{
+            nodeIntegration: false,
+            preload: path.join(__dirname, '../../preload.js'),
+            contextIsolation: true
+        }
+    })
+
+    win.loadFile(path.join(__dirname, '../../public/index.html'));
+}
+
+app.whenReady().then(()=>{
+    createWindow()
+
+    app.on('activate', ()=>{
+        if(BrowserWindow.getAllWindows().length===0) createWindow()
+    })
+})
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
